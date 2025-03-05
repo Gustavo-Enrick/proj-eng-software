49436eb7aceacbbb9e192a7286d80e2645c7f9b6
Gustavo Enrick <gustavoenrick16@gmail.com>
2025-03-05 20:46:16 -0300

    feat: Added class voter

---
+++ voter.js
@@ -0,0 +1,27 @@
+class Voter{
+    constructor (color,candidate)
+    {
+        this.color = color;
+        this.candidate = candidate;
+    }
+
+    getColor() 
+    {
+        return this.color;
+    }
+
+    setColor(color)
+    {
+        this.color = color;
+    }
+
+    getCandidate()
+    {
+        return this.candidate;
+    }
+
+    setCandite(candidate)
+    {
+        this.candidate = this.candidate;
+    }
+}
